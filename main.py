e709e0c97bdbbba81b964ddccbbfa2c3079c614f
Moeazed <76668184+moeazed@users.noreply.github.com>
2024-05-08 02:02:47 -0400

    added a feature to return the top 3 articles about said stock

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ STOCK_NAME = "NVDA"
 COMPANY_NAME = "NVIDIA"
 
 STOCK_API_KEY = "1YHTV3RHWX9838SS"
+NEWS_API_KEY = "dfed282e5a084a0c93fa35e8c7042e5e"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
@@ -25,6 +26,17 @@ yesterday_closing_price = yesterday_data["4. close"]
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+diff_percent = (difference / float(yesterday_closing_price)) * 100
 
+if diff_percent > 1:
+    news_params = {
+        "apiKey" : NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+    print(three_articles)
 
 
